765797182f425900b9d0edc6a15621ddcc99c44e
Yulia <JuliaKuz@yandex.ru>
2023-09-03 16:20:30 +0300

    Push-Sender

---
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,40 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val messageLike = Message.builder()
+        .putData("action", "LIKE")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    val messagePost = Message.builder()
+        .putData("action", "POST")
+        .putData("content", """{          
+          "postId": 5,
+          "postAuthor": "Netology",
+          "postContent": "Освоение новой профессии — это не только открывающиеся возможности и перспективы, но и настоящий вызов самому себе. Приходится выходить из зоны комфорта и перестраивать привычный образ жизни: менять распорядок дня, искать время для занятий, быть готовым к возможным неудачам в начале пути."
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(messagePost)
+}
